fd64bc51d3af298baf40aa512ac10af3dcd0ca52
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-15 11:41:23 -0500

    updated server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 const knex = require('knex')
+const cors = require('cors');
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL
 });
+
+app.use(
+  cors()
+);
+
 
 app.set('db', db);
 
