1e3e60398f2dd17d6659561a1a131494484155c8
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-16 12:54:28 -0500

    updated error-handler.js

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,4 +1,4 @@
-const { NODE_ENV } = require('../config')
+const { NODE_ENV } = require('../config');
 
 module.exports = function errorHandler(error, req, res, next) {
   const response = (NODE_ENV === 'production')
@@ -6,4 +6,4 @@ module.exports = function errorHandler(error, req, res, next) {
     : (console.error(error), { error: error.message, details: error })
 
   res.status(500).json(response)
-}
+};
