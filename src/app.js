9102d883bf2f8edd5240633822900faec9673a55
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-16 12:32:41 -0500

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const errorHandler = require('./middleware/error-handler')
@@ -9,18 +9,30 @@ const postRecipeRouter = require('./postrecipe/postrecipe-router')
 const recipesRouter = require('./recipes/recipes-router')
 const app = express()
 
+const allowedOrigins = ['http://localhost:3000', 'https://bespoke-cilent.vercel.app/']
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(helmet()),
+app.use(helmet())
 
 
+
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not' + 'allow access from the specified Origin'
+        return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+}}))
 
 //testing endpoint
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Welcome to Bespoke Server!')
 })
 // path '/postrecipe' , route handler function postRecipeRouter
 app.use('/postrecipe', postRecipeRouter)
