015e2c0a8e0d5611bc7858dac475fce76bcf4c8f
RX-78GP02A <mridley@knights.ucf.edu>
2020-12-16 15:24:44 -0500

    updated test

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 //testing endpoint
 app.get('/', (req, res) => {
-  res.send('Welcome to Bespoke Server!')
+  res.send('Welcome to Bespoke Server')
 });
 // path '/postrecipe' , route handler function postRecipeRouter
 app.use('/postrecipe', postRecipeRouter);
